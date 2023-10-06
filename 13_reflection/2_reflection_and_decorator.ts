7f3232fc1289566e39c8e921414a13cdc5ef6c12
nextorm77 <90032559+nextorm77@users.noreply.github.com>
2023-10-06 20:32:22 +0900

    Update 2_reflection_and_decorator.ts

--- 13_reflection/2_reflection_and_decorator.ts
+++ 13_reflection/2_reflection_and_decorator.ts
@@ -1,76 +1,101 @@
 /**
  * Reflection and Decorator
  */
+// 데코레이터 실행 환경 편의를 위해
+// nest.js 프레임워크에서 실행
+
+// 특이한 import 방식?
 import 'reflect-metadata';
 
 const restrictParamValueKey = Symbol('restrictParamValue');
 
-interface RestrictionInfo<T>{
-    index: number;
-    restrictedValues: T[];
+interface RestrictionInfo<T> {
+  index: number; // 함수 인수 배열(args)의 index와 일치
+  restrictedValues: T[];
 }
 
-function RestrictParamValue<T>(restrictedValues: T[]){
-    return (target: any, propertyKey: string, index: number) => {
-        const prevMeta = Reflect.getOwnMetadata(restrictParamValueKey, target, propertyKey) ?? [];
+// 해당 데코레이터가 선언된 클래스 선언만으로 최초 1회 호출?
+function RestrictParamValue<T>(restrictedValues: T[]) {
+  return (target: any, propertyKey: string, index: number) => {
+    // 기존 배열 가져오기
+    const prevMeta: RestrictionInfo<any>[] =
+      Reflect.getOwnMetadata(restrictParamValueKey, target, propertyKey) ?? [];
 
-        const info: RestrictionInfo<T> = {
-            index,
-            restrictedValues,
-        }
+    // 신규 배열 요소 생성
+    // 상단 언급 제네릭 T를 활용
+    const info: RestrictionInfo<T> = {
+      index,
+      restrictedValues,
+    };
 
-        Reflect.defineMetadata(restrictParamValueKey, [
-            ...prevMeta,
-            info,
-        ], target, propertyKey);
+    // 기존 배열 + 신규 배열 요소
+    Reflect.defineMetadata(
+      restrictParamValueKey,
+      [...prevMeta, info],
+      target,
+      propertyKey,
+    );
 
-        console.log(Reflect.getOwnMetadata(restrictParamValueKey, target, propertyKey));
-    }
+    console.log(
+      Reflect.getOwnMetadata(restrictParamValueKey, target, propertyKey),
+    );
+  };
 }
 
-function ValidateMethod(target: any, propertyKey: string, descriptor: PropertyDescriptor){
-    const metas: RestrictionInfo<any>[] = Reflect.getOwnMetadata(restrictParamValueKey, 
-        target, propertyKey) ?? [];
+// 해당 데코레이터가 선언된 클래스 선언만으로 최초 1회 호출?
+function ValidateMethod(
+  target: any,
+  propertyKey: string,
+  descripter: PropertyDescriptor,
+) {
+  const metas: RestrictionInfo<any>[] =
+    Reflect.getOwnMetadata(restrictParamValueKey, target, propertyKey) ?? [];
 
-    const original = descriptor.value;
+  const original = descripter.value;
 
-    descriptor.value = function(...args: any){
-        const invalids = metas.filter(
-            (x) => !x.restrictedValues.includes(args[x.index])
-        );
+  // 화살표 함수는 최신 기법(자신만의 this 생성X)이어서
+  // descripter.value(구 문법?)와 호환X?
+  // 실행시 작동 코드
+  descripter.value = function (...args: any) {
+    const invalids = metas.filter(
+      (x) => !x.restrictedValues.includes(args[x.index]),
+    );
 
-        if(invalids.length > 0){
-            throw Error(`잘못된 값입니다. ${invalids.map(x => args[x.index]).join(', ')}`)
-        }
-
-        return original.apply(this, args);
+    if (invalids.length > 0) {
+      throw Error(
+        `잘못된 값입니다. ${invalids.map((x) => args[x.index]).join(', ')}`,
+      );
     }
+    // this는 original 객체 자신?
+    return original.apply(this, args);
+  };
+  //  console.log('메소드 데코레이터 선언');
 }
 
 class Idol {
-    name: string;
-    age: number;
-
-    constructor(name: string, age: number) {
-        this.name = name;
-        this.age = age;
-    }
-
-    @ValidateMethod    
-    // style에는 '신나게' | '열정적으로'
-    sing(@RestrictParamValue(['신나게', '열정적으로']) style: string, 
-    @RestrictParamValue([1,2,3]) ranking: number) {
-        // if(style !== '신나게' && style !== '열정적으로'){
-        //     throw Error('안됨');
-        // }
-
-        return `${this.name}이 ${style} 노래를 부릅니다.`
-    }
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  @ValidateMethod
+  sing(
+    @RestrictParamValue(['신나게', '열정적으로']) style: string,
+    @RestrictParamValue([1, 2, 3]) ranking: number,
+  ) {
+    return `${this.name}이(가) ${style} 노래를 부릅니다.`;
+  }
 }
 
-const yuJin = new Idol('안유진', 23);
-
-console.log('--- sing ---');
-console.log(yuJin.sing('신나게', 1));
-console.log(yuJin.sing('열정적으로', 2));
-console.log(yuJin.sing('기분 나쁘게', 3));
+@Injectable()
+export class AppService {
+  getHello(): string {
+    // const yuju = new Idol('유주', 27);
+    console.log('--- sing ---');
+    // console.log(yuju.sing('신나게', 1));
+    // console.log(yuju.sing('열정적으로', 2));
+    // console.log(yuju.sing('기분나쁘게', 4));
+    return 'Hello World';
+  }
+}
