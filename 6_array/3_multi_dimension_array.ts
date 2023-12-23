b44583147a5aef43908ac6a8d7ebe521f15040f4
nextorm77 <90032559+nextorm77@users.noreply.github.com>
2023-12-23 21:49:40 +0900

    Update 3_multi_dimension_array.ts

--- 6_array/3_multi_dimension_array.ts
+++ 6_array/3_multi_dimension_array.ts
@@ -22,17 +22,17 @@
 const numb2DArr: number[][] = [
     [1, 2, 3],
     [4, 5, 6]
-]
+] // 타입(number[][]) 지정하지 않아도 해당 타입으로 유추
 
 const str2DArr = [
     ['1', '2', '3'],
     ['4', '5', '6'],
-]
+] // string[][] 타입으로 유추
 
 const strAndNumbArr: (number | string)[][] = [
     [1, '2', 3],
     ['4', 5, '6'],
-]
+] // 타입((number | string)[][]) 선언하지 않아도 해당 타입으로 유추
 
 let strArrOrNumbArr: string[][] | number[][] = [
     [1, 2, 3],
