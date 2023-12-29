aa2e6bf01356d4e45dbafd4e51a9da46ccdc682d
nextorm77 <90032559+nextorm77@users.noreply.github.com>
2023-12-29 12:46:12 +0900

    Update 3_property_initialization.ts

--- 9_class/3_property_initialization.ts
+++ 9_class/3_property_initialization.ts
@@ -9,7 +9,7 @@ class Person {
     // optional 값 선언법
     pet?: string;
     // type of undefined 선언법
-    petAge: number | undefined;
+    petAge: number | undefined; // undefined가 없는 경우, 초기값 제공 또는 constructor에 명시 필요, 아니면 문법 오류
 
     constructor(name: string, pet?: string){
         this.name = name;
@@ -18,6 +18,8 @@ class Person {
 }
 
 class RouteStack{
+    // TS가 인지하는 초기화(생성자 명시, 초기값 할당)가 아닌 다른 방식(여기에선 initialize함수 이용)으로
+    // 프로퍼티 초기화를 보장하는 경우, TS에게 !(느낌표)로 인식시켜줌
     stack!: string[];
 
     constructor(){
