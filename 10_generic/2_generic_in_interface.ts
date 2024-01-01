882fcf72a30022171ef53a691b3afe4df8a593ab
nextorm77 <90032559+nextorm77@users.noreply.github.com>
2024-01-01 11:28:28 +0900

    Update 2_generic_in_interface.ts

--- 10_generic/2_generic_in_interface.ts
+++ 10_generic/2_generic_in_interface.ts
@@ -6,20 +6,22 @@ interface Cache<T> {
     lastUpdate: Date;
 }
 
+// 당연한 얘기지만, interface에선 "<타입>" 표시 생략 불가?
 const cache1: Cache<string> = {
     data: ['data1', 'data2'],
     lastUpdate: new Date(),
 }
 
-// const cach2: Cache<number> = {
-//     data: [123, 456],
-//     lastUpdate: new Date(),
-// }
+const cach2: Cache<number> = {
+    data: [123, 456],
+    lastUpdate: new Date(),
+}
 
 interface DefaultGeneric<T = string>{
     data:T[];
 }
 
-// const cache3: DefaultGeneric = {
-//     data: [123, 456],
-// }
+// "<타입>" 표시하지 않으면 string을 기본 타입으로 인식
+const cache3: DefaultGeneric = {
+    data: [123, 456], // 디폴트가 string 이므로 오류 발생
+}
