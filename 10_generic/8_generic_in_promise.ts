46a205af94a289bd8707bfbe761e376fc9585f53
nextorm77 <90032559+nextorm77@users.noreply.github.com>
2024-01-06 00:35:28 +0900

    Update 8_generic_in_promise.ts

--- 10_generic/8_generic_in_promise.ts
+++ 10_generic/8_generic_in_promise.ts
@@ -10,7 +10,7 @@ const afterTwoSeconds = function () {
 }
 
 const runner = async function () {
-    const res = await afterTwoSeconds();
+    const res = await afterTwoSeconds(); // res: unkown 으로 표출 => 문맥상 string으로 표출되어야 함 => 이것을 아래 코드에서 개선
     console.log(res);
 }
 
@@ -25,12 +25,14 @@ const afterOneSecond = function(): Promise<string>{
 }
 
 const runner2 = async function () {
-    const res = await afterOneSecond();
+    const res = await afterOneSecond(); // res: string 으로 표출
     console.log(res);
 }
 
 runner2();
 
+// runner3: () => Promise<string>
+// async 키워드를 쓰기만 하면 비동기 함수가 없어도 Promise 타입을 반환하는 것으로 표출
 const runner3 = async function(){
     return 'string return';
 }
