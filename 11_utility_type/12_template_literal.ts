934f247b3abeb7d574485a6877dca8a19c59393b
nextorm77 <90032559+nextorm77@users.noreply.github.com>
2024-01-06 18:59:15 +0900

    Update 12_template_literal.ts

--- 11_utility_type/12_template_literal.ts
+++ 11_utility_type/12_template_literal.ts
@@ -5,12 +5,16 @@ type CodeFactory = 'Code Factory';
 
 // Uppercase
 type CodeFactoryUpper = Uppercase<CodeFactory>;
+// type CodeFactoryUpper = 'CODE FACTORY'; // 결과
 
 // Lowercase
 type CodeFactoryLower = Lowercase<CodeFactoryUpper>;
+// type CodeFactoryLower = 'code factory'; // 결과
 
 // Capitalize
 type CodeFactoryCapital = Capitalize<CodeFactoryLower>;
+// type CodeFactoryCapital = 'Code Factory'; // 결과
 
 // Uncapitalize
 type CodeFactoryUnCapital = Uncapitalize<CodeFactoryCapital>;
+// type CodeFactoryUnCapital = 'code factory'; // 결과
