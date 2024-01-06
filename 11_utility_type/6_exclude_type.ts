e60a542c9e6c145b158f89699649e85e1ce06ea6
nextorm77 <90032559+nextorm77@users.noreply.github.com>
2024-01-06 09:25:38 +0900

    Update 6_exclude_type.ts

--- 11_utility_type/6_exclude_type.ts
+++ 11_utility_type/6_exclude_type.ts
@@ -3,5 +3,7 @@
  */
 
 type NoString = Exclude<string | boolean | number, string>;
+// type NoString = boolean | number; // 위 코드와 동일
 
-type NoFunction = Exclude<string | (() => void), Function>;
+type NoFunction = Exclude<string | (() => void), Function>; // '() => void' 양쪽에 반드시 괄호 필요
+// type NoFunction = string; // 위 코드와 동일
