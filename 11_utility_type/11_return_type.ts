e06320eddb2097308747b4ef99177c183923bdc7
nextorm77 <90032559+nextorm77@users.noreply.github.com>
2024-01-06 18:04:50 +0900

    Update 11_return_type.ts

--- 11_utility_type/11_return_type.ts
+++ 11_utility_type/11_return_type.ts
@@ -2,7 +2,9 @@
  * Return Type
  */
 type ReturnTypeSample = ReturnType<()=> number>;
+// type ReturnTypeSample = number; // 결과
 
 type FunctionSign = (x: number, y: number) => number;
 
 type ReturnType2 = ReturnType<FunctionSign>;
+// type ReturnType2 = number; // 결과
