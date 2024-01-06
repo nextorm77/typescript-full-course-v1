c54fcada3ad16b0819e1b96de286956f1822359d
nextorm77 <90032559+nextorm77@users.noreply.github.com>
2024-01-06 09:58:12 +0900

    Update 9_parameter_type.ts

--- 11_utility_type/9_parameter_type.ts
+++ 11_utility_type/9_parameter_type.ts
@@ -7,4 +7,6 @@ function sampleFunction(x: number, y: string, z: boolean){
 }
 
 type Params = Parameters<typeof sampleFunction>;
+// type Params = [x: number, y:string, z:boolean]; // 위 코드와 동일
 type Params2 = Parameters<(one: number) => void>;
+// type Params2 = [one: number]; // 위 코드와 동일
