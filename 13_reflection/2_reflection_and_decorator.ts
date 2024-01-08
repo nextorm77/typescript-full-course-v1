3bcde1c401c6b5ad16af5f4297dc8a8f859cb650
nextorm77 <90032559+nextorm77@users.noreply.github.com>
2024-01-08 15:17:07 +0900

    Update 2_reflection_and_decorator.ts

--- 13_reflection/2_reflection_and_decorator.ts
+++ 13_reflection/2_reflection_and_decorator.ts
@@ -17,8 +17,8 @@
 // npm install --save-dev reflect-metadata
 
 // 코드 이해 순서
-// RestrictParamValue 파라메터 데코레이터만 먼저 실행 분석 후 
-// ValidateMethod 메소드 데코레이터 실행 분석
+// 1. "RestrictParamValue 파라메터 데코레이터"만 먼저 실행 분석 후 
+// 2. "ValidateMethod 메소드 데코레이터" 실행 분석
 
 // 특이한 import 방식?
 import 'reflect-metadata';
